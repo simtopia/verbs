7d64075141fa50ebae21294699a3e7d5d43c1761
Jordan <jordan@simtopia.ai>
2023-08-02 16:18:31 +0100

    Clean up utils

--- examples/aave/agents/mod.rs
+++
@@ -1,7 +0,0 @@
-mod borrow_agent;
-mod liquidation_agent;
-mod price_admin_agent;
-
-pub use borrow_agent::BorrowAgent;
-pub use liquidation_agent::LiquidationAgent;
-pub use price_admin_agent::PriceAdminAgent;
--- src/utils.rs
+++ src/utils.rs
@@ -4,8 +4,9 @@ use revm::primitives::Address as RevmAddress;
 use revm::primitives::U256 as RevmU256;
 use std::fs::File;
 
+/// Write a vector series of agent values to a csv file
 pub fn csv_writer<T: ToString>(records: &Vec<Vec<T>>, output_path: &str) {
-    let output_file = File::create(output_path).unwrap();
+    let output_file = File::create(output_path).expect("Could not open csv file");
 
     let mut wtr = Writer::from_writer(output_file);
 
@@ -17,37 +18,44 @@ pub fn csv_writer<T: ToString>(records: &Vec<Vec<T>>, output_path: &str) {
     wtr.flush().expect("Error flushing csv");
 }
 
+/// Create a revm address from a hex string
 pub fn address_from_hex(x: &str) -> RevmAddress {
-    let address = x.strip_prefix("0x").unwrap();
-    let address = hex::decode(address).expect("Decoding failed");
+    let address = x.strip_prefix("0x").expect("Addresses require '0x' prefix");
+    let address = hex::decode(address).expect("Decoding hex string failed");
     RevmAddress::from_slice(address.as_slice())
 }
 
+/// Create a Bytes object from a hex string
 pub fn data_bytes_from_hex(hx: &str) -> Bytes {
-    let data = hex::decode(hx).expect("Decoding failed");
+    let data = hex::decode(hx).expect("Decoding hex failed");
     Bytes::from(data)
 }
 
+/// Casting between revm and ethers-core types
 pub trait Cast<Y> {
     fn cast(self) -> Y;
 }
 
+/// Cast etheres-core address to revm address
 impl Cast<Address> for RevmAddress {
     fn cast(self) -> Address {
         Address::from(self.0)
     }
 }
 
+/// Cast revm address to ethers-core address
 impl Cast<RevmAddress> for Address {
     fn cast(self) -> RevmAddress {
         RevmAddress::from(self.0)
     }
 }
 
+/// Convert values in in eth to weth
 pub trait Eth {
     fn to_weth(x: u128) -> Self;
 }
 
+/// Convert revm u256 from eth to weth value
 impl Eth for RevmU256 {
     fn to_weth(x: u128) -> Self {
         let x: u128 = x * 10u128.pow(18);
@@ -55,9 +63,27 @@ impl Eth for RevmU256 {
     }
 }
 
+/// Convert ethers-core u256 from eth to weth value
 impl Eth for U256 {
     fn to_weth(x: u128) -> Self {
         let x: u128 = x * 10u128.pow(18);
         Self::from(x)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn cast_addresses() {
+        let w = RevmAddress::random();
+        let x = w.cast();
+        let y = x.cast();
+        let z = y.cast();
+
+        assert_eq!(w, y);
+        assert_eq!(x, z);
     }
 }
