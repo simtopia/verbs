da04519b9ff99a8e3ffaf08bc0cd5bba4571b9e9
Jordan <jordan@simtopia.ai>
2023-11-28 22:46:08 +0000

    Use ABI in tests

--- tests/test_empty_env/test_env.py
+++ tests/test_empty_env/test_env.py
@@ -1,7 +1,6 @@
-import eth_abi
 import pytest
 
-from verbs import EmptyEnv, utils
+from verbs import EmptyEnv, abi, utils
 
 INITIAL_VALUE = 101
 
@@ -50,26 +49,30 @@ def test_deploy_and_call_contract(env, bytecode, constructor_args, test_abi):
     # Deploy contract and get address
     address = env.deploy_contract("test_contract", bytecode + constructor_args)
 
+    # Init contract abi
+    a = abi.get_abi("ABI", test_abi)
+
     # Get current contract value
-    call_0 = utils.encode_function_args(test_abi[2], [], [])
+    call_0 = a.getValue.encode([])
     result_0 = env.call(env.admin_address, address, call_0, 0)
-    result_0 = eth_abi.decode(["int256"], bytes(result_0[0]))
+    result_0 = a.getValue.decode(result_0[0])
+
     assert result_0[0] == INITIAL_VALUE
 
     # Update contract value
-    call_1 = utils.encode_function_args(test_abi[3], ["int256"], [202])
+    call_1 = a.setValue.encode([202])
     _, logs, _ = env.execute(env.admin_address, address, call_1, 0)
 
     assert len(logs) == 1
 
-    log_value = eth_abi.decode(["int256", "int256"], bytes(logs[0][1]))
+    log_value = a.ValueUpdated.decode(logs[0][1])
 
     assert log_value == (101, 202)
 
     # Get new contract value
-    call_2 = utils.encode_function_args(test_abi[2], [], [])
+    call_2 = a.getValue.encode([])
     result_2 = env.call(env.admin_address, address, call_2, 0)
-    result_2 = eth_abi.decode(["int256"], bytes(result_2[0]))
+    result_2 = a.getValue.decode(result_2[0])
     assert result_2[0] == 202
 
 
@@ -78,14 +81,17 @@ def test_sim_update(env, bytecode, constructor_args, test_abi):
     # Deploy contract and get address
     address = env.deploy_contract("test_contract", bytecode + constructor_args)
 
+    # Init contract abi
+    a = abi.get_abi("ABI", test_abi)
+
     # Get current contract value
-    call_0 = utils.encode_function_args(test_abi[2], [], [])
+    call_0 = a.getValue.encode([])
     result_0 = env.call(env.admin_address, address, call_0, 0)
-    result_0 = eth_abi.decode(["int256"], bytes(result_0[0]))
+    result_0 = a.getValue.decode(result_0[0])
     assert result_0[0] == INITIAL_VALUE
 
     # Submit contract update call
-    call_1 = utils.encode_function_args(test_abi[3], ["int256"], [202])
+    call_1 = a.setValue.encode([202])
     env.submit_call(env.admin_address, address, call_1, True)
 
     env.process_block()
@@ -93,7 +99,7 @@ def test_sim_update(env, bytecode, constructor_args, test_abi):
     assert env.step == 1
 
     # Get new value after block update
-    call_2 = utils.encode_function_args(test_abi[2], [], [])
+    call_2 = a.getValue.encode([])
     result_2 = env.call(env.admin_address, address, call_2, 0)
-    result_2 = eth_abi.decode(["int256"], bytes(result_2[0]))
+    result_2 = a.getValue.decode(result_2[0])
     assert result_2[0] == 202
