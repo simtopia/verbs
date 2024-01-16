4a431493bcb24115c3b3c1f873fa4dc1270f829b
Marc Sabaté <msabvid@gmail.com>
2024-01-16 10:27:44 +0000

    conftest updated

--- tests/conftest.py
+++ tests/conftest.py
@@ -48,52 +48,9 @@ def test_abi():
                 {
                     "indexed": True,
                     "internalType": "address",
-                    "name": "collateralAsset",
+                    "name": "by",
                     "type": "address",
                 },
-                {
-                    "indexed": True,
-                    "internalType": "address",
-                    "name": "debtAsset",
-                    "type": "address",
-                },
-                {
-                    "indexed": True,
-                    "internalType": "address",
-                    "name": "user",
-                    "type": "address",
-                },
-                {
-                    "indexed": False,
-                    "internalType": "uint256",
-                    "name": "debtToCover",
-                    "type": "uint256",
-                },
-                {
-                    "indexed": False,
-                    "internalType": "uint256",
-                    "name": "liquidatedCollateralAmount",
-                    "type": "uint256",
-                },
-                {
-                    "indexed": False,
-                    "internalType": "address",
-                    "name": "liquidator",
-                    "type": "address",
-                },
-                {
-                    "indexed": False,
-                    "internalType": "bool",
-                    "name": "receiveAToken",
-                    "type": "bool",
-                },
-            ],
-            "name": "LiquidationCall",
-            "type": "event",
-        },
-        {
-            "anonymous": False,
-            "inputs": [
                 {
                     "indexed": False,
                     "internalType": "int256",
