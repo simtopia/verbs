da79cc9e850e436e8a5ca19b2daaa800d16d694c
zombie-einstein <13398815+zombie-einstein@users.noreply.github.com>
2024-02-09 21:43:34 +0000

    Rewrite batch-runner

--- tests/test_batch_runner.py
+++ tests/test_batch_runner.py
@@ -5,32 +5,43 @@ def test_batch_runner(bytecode, constructor_args, test_abi, agent_type):
 
     test_abi = abi.get_abi("TEST", test_abi)
 
-    def init_func(_bytecode, _constructor_args):
-        env = envs.EmptyEnv(1234)
-        admin = utils.int_to_address(99)
-        env.create_account(admin, int(1e19))
-        address = env.deploy_contract(
-            admin, "test_contract", _bytecode + _constructor_args
-        )
-        return env.export_snapshot(), address
+    env = envs.EmptyEnv(1234)
+    admin = utils.int_to_address(99)
+    env.create_account(admin, int(1e19))
+    contract_address = env.deploy_contract(
+        admin, "test_contract", bytecode + constructor_args
+    )
+    snapshot = env.export_snapshot()
 
-    def exec_func(snapshot, n_steps, seed, contract_address):
-        env = envs.EmptyEnv(seed, snapshot)
-        agent = agent_type(1, contract_address, test_abi)
+    def sim_func(env, seed, n_steps, *, address, abi, offset):
+        agent = agent_type(1, address, abi, offset=offset)
         sim_runner = sim.Sim(seed, env, [agent])
         results = sim_runner.run(n_steps)
         return results
 
-    b = batch_runner.BatchRunner(init_func, exec_func)
-
-    batch_results = b.run(
-        10,
-        3,
-        [dict(), dict()],
-        init_kwargs=dict(_bytecode=bytecode, _constructor_args=constructor_args),
+    batch_results = batch_runner.batch_run(
+        sim_func,
+        n_steps=10,
+        n_samples=2,
+        parameters_samples=[dict(offset=0), dict(offset=0), dict(offset=1)],
+        snapshot=snapshot,
         n_jobs=1,
+        address=contract_address,
+        abi=test_abi,
     )
 
-    assert len(batch_results) == 2
+    assert isinstance(batch_results, list)
+    assert len(batch_results) == 3
+    assert batch_results[0]["params"] == batch_results[1]["params"]
+    assert batch_results[0]["params"] == dict(offset=0)
     # We ran with the same parameters, so should get the same results
     assert batch_results[0]["samples"] == batch_results[1]["samples"]
+    assert len(batch_results[0]["samples"]) == 2
+    assert len(batch_results[0]["samples"][0]) == 10
+
+    assert batch_results[0]["params"] != batch_results[2]["params"]
+    assert batch_results[2]["params"] == dict(offset=1)
+    # But different parameters should differ
+    assert batch_results[1]["samples"] != batch_results[2]["samples"]
+    assert len(batch_results[2]["samples"]) == 2
+    assert len(batch_results[2]["samples"][0]) == 10
